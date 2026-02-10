8848c5736f757578f3e1deaab243274205279f61
shy3939 <kpky33@naver.com>
2026-02-10 17:31:47 +0900

    [🎉 add: 유시환] Monster파일 생성
    일단 몬스터 폴더를 만들고 그 안에 AMonsterCharacter를 생성한 상태입니다.
    이후 어떻게 코드를 작성할지는 특강과 챌린지 반 영상을 보면서 생각중입니다.

---
+++ Source/FinalMinutes/Private/Monster/AMonsterCharacter.cpp
@@ -0,0 +1,22 @@
+﻿#include "FinalMinutes/Public/AMonsterCharacter.h"
+
+AAMonsterCharacter::AAMonsterCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AAMonsterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AAMonsterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AAMonsterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/FinalMinutes/Public/Monster/AMonsterCharacter.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "AMonsterCharacter.generated.h"
+
+UCLASS()
+class FINALMINUTES_API AAMonsterCharacter : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	AAMonsterCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
