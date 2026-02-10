55f390559f2745e5a3947a2a21a717501e9ab1fb
JongKyu <kongi7064@gmail.com>
2026-02-10 19:11:15 +0900

    [🎉 add : 홍종규] PlayerCharacterController 생성

---
+++ Source/FinalMinutes/Public/Controller/PlayerCharacterController.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PlayerCharacterController.generated.h"
+
+UCLASS()
+class FINALMINUTES_API APlayerCharacterController : public APlayerController
+{
+	GENERATED_BODY()
+};
