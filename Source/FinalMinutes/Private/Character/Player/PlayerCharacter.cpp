f0acee70014d76f92024497f7ca2c90b90910226
JongKyu <kongi7064@gmail.com>
2026-02-10 17:25:55 +0900

    [✨ update : 홍종규] AttributeSet, ASC설정
    PlayerCharacter에 AttributeSet, ASC기본 설정

--- Source/FinalMinutes/Private/Character/Player/PlayerCharacter.cpp
+++ Source/FinalMinutes/Private/Character/Player/PlayerCharacter.cpp
@@ -1,13 +1,37 @@
 #include "Character/Player/PlayerCharacter.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystem/Attributes/CharacterAttributeSet.h"
+#include "AbilitySystem/Attributes/SensorAttributeSet.h"
 
 APlayerCharacter::APlayerCharacter()
 {
-	// 초기화 작업
+	// ASC 달아주기
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	
+	// 사용할 AttributeSet 설정
+	CharacterAttributeSet = CreateDefaultSubobject<UCharacterAttributeSet>(TEXT("CharacterAttributeSet"));
+	SensorAttributeSet = CreateDefaultSubobject<USensorAttributeSet>(TEXT("SensorAttributeSet"));
+}
+
+UAbilitySystemComponent* APlayerCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
 }
 
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+	
+	// ASC초기화
+	InitializeAbilitySystem();
+}
+
+void APlayerCharacter::InitializeAbilitySystem()
+{
+	if (AbilitySystemComponent)
+	{
+		AbilitySystemComponent->InitAbilityActorInfo(this, this);
+	}
 }
 
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
--- Source/FinalMinutes/Public/Character/Player/PlayerCharacter.h
+++ Source/FinalMinutes/Public/Character/Player/PlayerCharacter.h
@@ -2,20 +2,35 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
 #include "PlayerCharacter.generated.h"
 
+class UAbilitySystemComponent;
+class UCharacterAttributeSet;
+class USensorAttributeSet;
+
 UCLASS()
-class FINALMINUTES_API APlayerCharacter : public ACharacter
+class FINALMINUTES_API APlayerCharacter : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 public:
 	APlayerCharacter();
 	
+	virtual class UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	virtual void InitializeAbilitySystem();
+	
 protected:
 	virtual void BeginPlay() override;
-
-
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GAS")
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+	
+	UPROPERTY()
+	TObjectPtr<UCharacterAttributeSet> CharacterAttributeSet;
+    
+	UPROPERTY()
+	TObjectPtr<USensorAttributeSet> SensorAttributeSet;
 public:	
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 };
