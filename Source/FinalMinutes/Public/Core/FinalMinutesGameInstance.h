16789bcb1e6426da16e692c9d54040085a9fa878
Ouranos0902 <ouranos0902@gmail.com>
2026-02-10 21:03:00 +0900

    [🎉 add: ] 사운드,세이브 서브시스템 폴더 추가
    -Core/FinalMinutesGameInstance 추가
    -Subsystems/SaveSubsystem 추가
    -Subsystems/SoundSubsystem 추가

---
+++ Source/FinalMinutes/Public/Core/FinalMinutesGameInstance.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "FinalMinutesGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FINALMINUTES_API UFinalMinutesGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
--- Source/FinalMinutes/Public/Subsystems/ItemDataSubsystem.h
+++ Source/FinalMinutes/Public/Subsystems/ItemDataSubsystem.h
@@ -2,22 +2,25 @@
 
 #include "CoreMinimal.h"
 #include "GameplayTagContainer.h"
+#include "FinalMinutes/Public/Character/Enemy/MonsterDataAsset.h"
+#include "FinalMinutes/Public/Items/Weapons/WeaponDataAsset.h"
 #include "Subsystems/GameInstanceSubsystem.h"
-#include "ItemDataSubsytem.generated.h"
+#include "ItemDataSubsystem.generated.h"
 
 
 UCLASS()
-class FINALMINUTES_API UItemDataSubsytem : public UGameInstanceSubsystem
+class FINALMINUTES_API UItemDataSubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
 	
 public:
-	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	UPROPERTY(EditAnywhere, Category = "ItemData")
+	TObjectPtr<UDataTable> WeaponDataTable;
+	UPROPERTY(EditAnywhere, Category = "MonsterData")
+	TObjectPtr<UDataTable> MonsterDataTable;
 	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ItemData)
-	UDataTable* MasterItemTable;
-	
-	UFUNCTION(BlueprintCallable, Category = ItemData)
-	class UWeaponDataAsset* GetWeaponData(FGameplayTag Tag);
-	
+	UFUNCTION(BlueprintCallable, Category = "ItemData")
+	FWeaponData GetWeaponData(FGameplayTag Tag);
+	UFUNCTION(BlueprintCallable, Category = "MonsterData")
+	FMonsterData GetMonsterData(FGameplayTag Tag);
 };
