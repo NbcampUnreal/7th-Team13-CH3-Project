7c270ecaab9ce68bfbe6db30689b3518fe211187
DulNae <moung051@naver.com>
2026-02-11 17:28:04 +0900

    [🎉 add: 박둘내] Interactable 인터페이스 생성

---
+++ Source/FinalMinutes/Public/Interfaces/InteractableInterface.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "InteractableInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UInteractableInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class FINALMINUTES_API IInteractableInterface
+{
+	GENERATED_BODY()
+
+public:
+	virtual void OnInteract(AActor* Interactor) = 0;
+};
