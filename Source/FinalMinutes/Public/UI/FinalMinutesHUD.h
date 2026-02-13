432b3f5f5b204e31ce328037a82f41a706b1061e
Ouranos0902 <ouranos0902@gmail.com>
2026-02-13 17:00:10 +0900

    [🎉 add: 허태린]FinalMinutesHUD 생성
    UI/FinalMinutesHUD 생성 UI폴더도 같이

---
+++ Source/FinalMinutes/Public/UI/FinalMinutesHUD.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "FinalMinutesHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FINALMINUTES_API AFinalMinutesHUD : public AHUD
+{
+	GENERATED_BODY()
+};
