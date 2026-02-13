9257654e58f9ac03b53962d08bca0607239f2e20
shy3939 <kpky33@naver.com>
2026-02-13 17:04:07 +0900

    [🎉 add: 유시환] Zombie클래스 생성
    AMonsterCharacter을 상속받는 Zombie클래스 추가

---
+++ Source/FinalMinutes/Private/Monster/Zombiee.cpp
@@ -0,0 +1,20 @@
+﻿#include "Monster/Zombiee.h"
+
+
+AZombiee::AZombiee()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AZombiee::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AZombiee::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+
---
+++ Source/FinalMinutes/Public/Monster/Zombiee.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Monster/AMonsterCharacter.h"
+#include "Zombiee.generated.h"
+
+UCLASS()
+class FINALMINUTES_API AZombiee : public AAMonsterCharacter
+{
+	GENERATED_BODY()
+
+public:
+	AZombiee();
+
+public:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+};
