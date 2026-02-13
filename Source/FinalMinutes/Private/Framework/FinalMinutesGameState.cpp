8174ddfe8e758834ae4ff272f09ac2f07c8bf170
Ouranos0902 <ouranos0902@gmail.com>
2026-02-13 16:11:57 +0900

    [🩹 fix: 허태린] 킬카운트 디버깅 로그 수정
    문자형->정수형

--- Source/FinalMinutes/Private/Framework/FinalMinutesGameState.cpp
+++ Source/FinalMinutes/Private/Framework/FinalMinutesGameState.cpp
@@ -21,6 +21,6 @@ void AFinalMinutesGameState::AddKill()
 	KillCount++;
 	OnKillCountChanged.Broadcast(KillCount);
 	//디버깅용
-	UE_LOG(LogTemp, Warning, TEXT("킬 카운트 테스트 : %s"), KillCount);
+	UE_LOG(LogTemp, Warning, TEXT("킬 카운트 테스트 : %d"), KillCount);
 }
 	
