6fc2fad0008e1a6f82b52326a44030d2946ecd5c
JongKyu <kongi7064@gmail.com>
2026-02-15 13:40:46 +0900

    [🎉 add : 홍종규] 재장전 기능 추가
    - 재장전 애니메이션 리타겟팅, Anim Montage 생성
    - 재장전 Gameplay Ability 생성 및 코드 작성
      - 서있을때, 앉아있을때, 엎드려있을때 각각 다른 재장전 Montage 부여
    - 재장전 Gameplay Effect 생성

---
+++ Source/FinalMinutes/Private/AbilitySystem/Abilities/GA_Reload.cpp
@@ -0,0 +1,131 @@
+#include "AbilitySystem/Abilities/GA_Reload.h"
+#include "Character/Player/PlayerCharacter.h"
+#include "AbilitySystemComponent.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "Abilities/Tasks/AbilityTask_WaitGameplayEvent.h"
+
+UGA_Reload::UGA_Reload()
+{
+	// 어빌리티가 실행될때 액터당 하나의 인스턴스(객체)만 생성해서 재사용하겠다.
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+
+	// Ability Tags, 어빌리티 자체에 붙여주는 태그
+	AbilityTags.AddTag(FGameplayTag::RequestGameplayTag(FName("Ability.Player.Reload")));
+	
+	// 이태그가 있으면 실행안함 
+	ActivationBlockedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("State.Player.IsReloading")));
+
+	// 소유태그 / 실행중 어떤 태그를 가질지
+	ActivationOwnedTags.AddTag(FGameplayTag::RequestGameplayTag(FName("State.Player.IsReloading")));
+}
+
+void UGA_Reload::ActivateAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	// 스킬 사용을 위한 조건 체크 , 쿨타임, 코스트다 있는지
+	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
+	{
+		// Commit 실패 (스태미너 부족, Cooldown 중 등)
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+	
+	UAbilitySystemComponent* MyASC = GetAbilitySystemComponentFromActorInfo();
+	UAnimMontage* SelectedMontage = ReloadMontage;
+	
+	if (MyASC->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("State.Player.IsProning"))))
+	{
+		SelectedMontage = ReloadProneMontage; 
+	}
+	else if (MyASC->HasMatchingGameplayTag(FGameplayTag::RequestGameplayTag(FName("State.Player.IsCrouching"))))
+	{
+		SelectedMontage = ReloadCrouchMontage;
+	}
+	
+	if (SelectedMontage)
+	{
+		UAbilityTask_PlayMontageAndWait* MontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
+			this,
+			NAME_None,
+			SelectedMontage
+			);
+		if (MontageTask)
+		{
+			MontageTask->OnBlendOut.AddDynamic(this, &UGA_Reload::OnMontageEnded);
+			MontageTask->OnInterrupted.AddDynamic(this, &UGA_Reload::OnMontageEnded);
+			MontageTask->OnCancelled.AddDynamic(this, &UGA_Reload::OnMontageEnded);
+			
+			MontageTask->ReadyForActivation();
+		}
+		else
+		{
+			EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		}
+	}
+	
+	
+	// Gameplay Event 대기
+	// 추가 옵션
+	UAbilityTask_WaitGameplayEvent* WaitEventTask =
+		UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(
+			this,
+			FGameplayTag::RequestGameplayTag(FName("Event.Montage.Reload"))
+		);
+
+	if (WaitEventTask)
+	{
+		WaitEventTask->EventReceived.AddDynamic(this, &UGA_Reload::OnReloadGameplayEvent);
+		WaitEventTask->ReadyForActivation();
+	}
+}
+
+void UGA_Reload::OnReloadGameplayEvent(FGameplayEventData EventData)
+{
+	APlayerCharacter* Character = Cast<APlayerCharacter>(GetAvatarActorFromActorInfo());
+	UAbilitySystemComponent* MyASC = GetAbilitySystemComponentFromActorInfo();
+
+	if (!Character || !MyASC) return;
+	
+	// 컨텍스트 생성, 효과가 어디서부터 나타났는지, 추후에 데미지 계산이나 로그 시스템에서 누가 사용한지 알 수 있음
+	FGameplayEffectContextHandle EffectContext = GetAbilitySystemComponentFromActorInfo()->MakeEffectContext();
+	EffectContext.AddSourceObject(Character);
+
+	// 실제로 적용될 효과를 작성, GE클래스
+	FGameplayEffectSpecHandle SpecHandle = GetAbilitySystemComponentFromActorInfo()->MakeOutgoingSpec(
+		ReloadEffectClass,
+		1.0f, // Level
+		EffectContext
+	);
+	
+	if (SpecHandle.IsValid())
+	{
+		// 나에게 발생하므로 나한테 효과 적용
+		ActiveReloadEffectHandle = MyASC->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
+	}
+}
+
+void UGA_Reload::OnMontageEnded()
+{
+	// 몽타주가 끝났으므로 어빌리티를 정상 종료
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
+
+void UGA_Reload::EndAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility,
+	bool bWasCancelled)
+{
+	if (ActiveReloadEffectHandle.IsValid())
+	{
+		GetAbilitySystemComponentFromActorInfo()->RemoveActiveGameplayEffect(ActiveReloadEffectHandle);
+		ActiveReloadEffectHandle.Invalidate();
+	}
+	
+	// 부모 클래스 호출 (Tag 제거 등)
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
