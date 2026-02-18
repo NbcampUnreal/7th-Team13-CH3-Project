ab623e84776403ee71eb4f428b3b3fd2cc097499
optimal <optimal9744@gmail.com>
2026-02-18 23:30:26 +0900

    [🎉 add: 최준혁 ] ProjectileBullet 클래스 추가

---
+++ Source/FinalMinutes/Private/Items/Projectiles/ProjectileBullet.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Projectiles/ProjectileBullet.h"
+
+
+// Sets default values
+AProjectileBullet::AProjectileBullet()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AProjectileBullet::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AProjectileBullet::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
