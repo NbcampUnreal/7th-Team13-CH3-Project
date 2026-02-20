a685ec8a4b96c694a14ecbcb9bd25806f3e74b14
shy3939 <kpky33@naver.com>
2026-02-20 14:30:43 +0900

    [✨ update: 유시환] Build.cs 수정

--- Source/FinalMinutes/FinalMinutes.Build.cs
+++ Source/FinalMinutes/FinalMinutes.Build.cs
@@ -8,7 +8,18 @@ public class FinalMinutes : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput"});
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"NavigationSystem",
+			"AIModule",
+			"Niagara",
+			"EnhancedInput",
+			"GameplayTasks"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks"});
 		PublicIncludePaths.AddRange(new string[]
