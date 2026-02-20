6a5d355c79eca4ee9cea171401459e7004db5a01
Ouranos0902 <ouranos0902@gmail.com>
2026-02-20 12:35:24 +0900

    [✨ update: 허태린]   GameMode - 게임 시작, 일시정지, 클리어, 오버, 나가기 함수 일부 구실
    GameMode - 게임 시작, 일시정지, 클리어, 오버, 나가기 함수 일부 구실

--- Source/FinalMinutes/Public/Framework/FinalMinutesGameMode.h
+++ Source/FinalMinutes/Public/Framework/FinalMinutesGameMode.h
@@ -11,4 +11,36 @@ class FINALMINUTES_API AFinalMinutesGameMode : public AGameModeBase
 	GENERATED_BODY()
 public:
 	AFinalMinutesGameMode();
+	protected:
+	
+	virtual void BeginPlay() override;
+	
+public:
+	
+	//수타트
+	UFUNCTION(BlueprintCallable, Category = "Game|Flow" )
+	void GameStart();
+	
+	//일시정지 인벤토리?
+	UFUNCTION(BlueprintCallable, Category = "Game|Flow" )
+	void GamePause(bool bIsPause);
+	
+	//게임 클리어
+	UFUNCTION(BlueprintCallable, Category = "Game|Flow" )
+	void GameClear();
+	
+	//게임 오버
+	UFUNCTION(BlueprintCallable, Category = "Game|Flow" )
+	void GameOver();
+	
+	//게임 나가기
+	UFUNCTION(BlueprintCallable, Category = "Game|Flow" )
+	void GameExit();
+	
+private:
+	
+	//타이머 관할 핸들러
+	FTimerHandle TimerHandle;
+	//생존시간 10분
+	float TimeLimit = 600.0f;
 };
