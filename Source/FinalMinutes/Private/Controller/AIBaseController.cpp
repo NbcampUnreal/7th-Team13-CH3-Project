ee685371e3368447b9ef4a0a2b25e347a2356b09
shy3939 <kpky33@naver.com>
2026-02-26 12:43:19 +0900

    [🚚 move: 유시환] 파일 이동

--- Source/FinalMinutes/Private/Controller/AIBaseController.cpp
+++ Source/FinalMinutes/Private/Controller/AIBaseController.cpp
@@ -29,11 +29,6 @@ void AAIBaseController::OnPossess(APawn* InPawn)
 	if (AAMonsterCharacter* PossessedMonster = Cast<AAMonsterCharacter>(InPawn))
 	{
 		PossessedMonster->bUseControllerRotationYaw = false;
-		
-		if (UFloatingPawnMovement* MovComp = Cast<UFloatingPawnMovement>(PossessedMonster->GetMovementComponent()))
-		{
-			/*MovComp->bOrientRotationToMovement = true; */
-		}
 	}
 	
 	GetWorldTimerManager().SetTimer(
@@ -63,18 +58,13 @@ void AAIBaseController::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 	APawn* MyPawn = GetPawn();
-	// 1. MyPawn이 유효하고 목표 회전값이 설정되었을 때만 실행
 	if (MyPawn && !TargetRotation.IsNearlyZero())
 	{
 		FRotator CurrentRot = MyPawn->GetActorRotation();
-        
-		// 2. RInterpTo를 사용하여 현재 각도에서 목표 각도까지 부드럽게 보간 (속도 10.0f)
 		FRotator SmoothRot = FMath::RInterpTo(CurrentRot, TargetRotation, DeltaTime, 10.0f);
-        
-		// MyPawn의 const를 제거하고 호출해야 에러가 나지 않습니다.
+		
 		MyPawn->SetActorRotation(SmoothRot);
-
-		// 3. 목표 각도에 거의 도달하면 계산 중단
+		
 		if (CurrentRot.Equals(TargetRotation, 1.0f))
 		{
 			TargetRotation = FRotator::ZeroRotator;
@@ -118,8 +108,7 @@ void AAIBaseController::MoveToRandomLocation()
 	if (bFound)
 	{
 		MoveToLocation(RandomLocation.Location);
-       
-		// 4. 즉시 회전시키는 대신, Tick에서 사용할 목표 각도만 업데이트합니다.
+		
 		FVector Direction = (RandomLocation.Location - GetPawn()->GetActorLocation()).GetSafeNormal();
 		if (!Direction.IsNearlyZero())
 		{
