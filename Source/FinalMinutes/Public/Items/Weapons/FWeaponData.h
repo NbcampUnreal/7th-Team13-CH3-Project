249f06b71b8989095358ae035e1f73fd1ea03cf1
shy3939 <kpky33@naver.com>
2026-02-28 13:13:23 +0900

    [✨ update: 유시환] 몬스터 피격 및 사망 기능 완료

--- Source/FinalMinutes/Public/Items/Weapons/FWeaponData.h
+++ Source/FinalMinutes/Public/Items/Weapons/FWeaponData.h
@@ -98,7 +98,7 @@ public:
 
     /** 기본 공격력 */
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon|Stats")
-    float DefaultDamage = 20.0f;
+    float DefaultDamage = 100.0f;
 
     /** 연사 가능 여부 */
     UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapon|Stats")
