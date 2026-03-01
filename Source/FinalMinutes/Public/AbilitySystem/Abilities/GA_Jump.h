1b4805f9f7fd55db92ba86da59aec6290b8296c7
JongKyu <kongi7064@gmail.com>
2026-03-01 15:52:35 +0900

    [🎉 add : 홍종규] GA_Jump 생성
    - GA_Jump 만든 이유
    1. 착지중에 Sprint키 누르고있으면 스태미너가 달아서 이것 수정함
    2. 착지중에 Sprint키 눌러도 스태미너 안담
    3. Sprint키 누르면서 점프하면 Sprint속도로 스태미너 소모 없이 이동가능
    4. 점프도 스태미너 달아야 한다고 생각해서 GA_Jump만듬
    5. GE에서 점프시 스태미너 10씩 감소

---
+++ Source/FinalMinutes/Public/AbilitySystem/Abilities/GA_Jump.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GA_Jump.generated.h"
+
+UCLASS()
+class FINALMINUTES_API UGA_Jump : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	UGA_Jump();
+	
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle, 
+		const FGameplayAbilityActorInfo* ActorInfo, 
+		const FGameplayAbilityActivationInfo ActivationInfo, 
+		const FGameplayEventData* TriggerEventData) override;
+	virtual void EndAbility(
+		const FGameplayAbilitySpecHandle Handle, 
+		const FGameplayAbilityActorInfo* ActorInfo, 
+		const FGameplayAbilityActivationInfo ActivationInfo, 
+		bool bReplicateEndAbility, 
+		bool bWasCancelled) override;
+	
+	UFUNCTION()
+	void OnLandedCallback(EMovementMode NewMovementMode);
+private:
+	UPROPERTY()
+	ACharacter* Character;
+};
