eefdaa7f59a6e7cebb0110622a85f5857222e681
shy3939 <kpky33@naver.com>
2026-03-04 11:18:14 +0900

    [🍻 test: 유시환] 발소리 사운드 테스트중

---
+++ Source/FinalMinutes/Public/AbilitySystem/AnimNotify/AN_MFootStep.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify_PauseClothingSimulation.h"
+#include "GameplayTagContainer.h"
+#include "AN_MFootStep.generated.h"
+
+
+UCLASS()
+class FINALMINUTES_API UAN_MFootStep : public UAnimNotify_PauseClothingSimulation
+{
+    GENERATED_BODY()
+public:
+    UAN_MFootStep();
+	
+    virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="GAS")
+    FGameplayTag FootStepCueTag;
+	
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="GAS")
+    float TraceDistance;
+};
